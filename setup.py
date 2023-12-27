3bc2e7c9fc90c2a4d4f32284bb5c0e2e10488ddb
demetri7 <demetri7@msu.edu>
2023-12-27 15:33:20 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="slots_sort",
+    version="1.0.0",
+    description="Sorts __slots__ attribute in python files",
+    license="MIT",
+    author="James Demetris",
+    packages=["slots_sort"],
+)
